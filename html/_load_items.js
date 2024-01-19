f500281f36790261bbb708af86d9fbabd732d12a
Jon Peirce <jon.peirce@gmail.com>
2024-01-19 11:49:50 +0000

    Working basic site with buttons and iframe
    - An iframe adds the content of the current item
    - The main body uses index.css and the item uses item.css (main difference is the background only for index page)
    - Simple pair of newer/older buttons that get disabled if we reach the newest and oldest
    - A pair of dummy New items

--- html/_load_items.js
+++ html/_load_items.js
@@ -1,10 +1,30 @@
-import news_items from "./news_items.json" assert { type: 'json' };;
+import all_items from "./news_items.json" assert { type: 'json' };;
 
 var ii = 0;
+var news_items = all_items.news;
+var newerBtn = document.getElementById("newer");
+var olderBtn = document.getElementById("older");
 
 // function to return news item at current index
 export function updateNewsItem(increment) {
     ii += increment;
-    var url = "items/"+news_items.news[ii].url;
+    // update index and next/prev buttons
+    if (ii <= 0) {
+        ii = 0;
+        olderBtn.disabled=false;
+        newerBtn.disabled=true;
+    }
+    else if (ii >= news_items.length-1) {
+        ii = news_items.length-1;
+        olderBtn.disabled=true;
+        newerBtn.disabled=false;
+    }
+    else {
+        olderBtn.disabled=false;
+        newerBtn.disabled=false;
+
+    }
+    //console.log(ii)
+    var url = "items/"+news_items[ii].url;
     document.getElementsByName('newsFrame')[0].src = url;
 }
